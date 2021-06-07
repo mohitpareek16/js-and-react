64881fa4a8653d5fa13b6b3e7c17fa92a25ad111
mohitpareek16 <83282138+mohitpareek16@users.noreply.github.com>
2021-06-07 10:43:08 +0530

    my Project mohit of 7 days bootcamp by shape ai

---
+++ src/components/Header.jsx
@@ -0,0 +1,11 @@
+import React from "react";
+
+function Header() {
+  return (
+    <Header>
+      <h1>H1</h1>
+    </Header>
+  );
+}
+
+export default Header;
---
+++ src/components/Note.jsx
@@ -0,0 +1,16 @@
+import React from "react";
+
+function Note() {
+  return (
+    
+      <div>
+        <h1>
+          JavaScript and React.js
+        </h1>
+        <p className="note">This was an amazing bootcamp taken up by Shaurya Sinha Sir. We covered everything from Scratch including Javascript, React.js, HTML.</p>
+      </div>
+    
+  );
+}
+
+export default Note;
